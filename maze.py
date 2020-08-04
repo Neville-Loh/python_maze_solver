45674472b0bedb46e5cd4fcc0a34ab7bbd8f4b4d
Neville <nevilleloh0115@gmail.com>
2020-08-05 11:47:49 +1200

    Added solution solving part

--- maze.py
+++ maze.py
@@ -122,10 +122,57 @@ class Maze():
 	def solve(self):
 		"""Finds a solution to maze, if one exists."""
 
+		# keep track of number of states explored
+		self.num_explored = 0
+
+		# Initialize frontier to just the starting position
+		start = Node(state=self.start, parent=None, action=None)
+		frontier = StackFrontier() #DFS
+		frontier.add(start)
+
+		# Initialize an empty explored set
+		self.explored = set()
+
+		#Keep looping until solution is found
+		while True:
+			# If nothing left in frontier, then no path
+			if frontier.empty():
+				raise Exception("No solution")
+
+			# Choose a node from the frontier
+			node = frontier.remove()
+			self.num_explored += 1
+
+			#If node is the goal, then is solution
+			if node.state == self.goal:
+				actions = []
+				cells = []
+
+				# Follow parent nodes to find solutions
+				while node.parent is not None:
+					actions.append(node.action)
+					cells.append(node.state)
+					node = node.parent
+				# Reverse the solution so it is readable
+				actions.reverse()
+				cells. reverse()
+				self.solution = (actions, cells)
+				return
+
+			# Not the goal
+			self.explored.add(node.state)
+
+			# Add neighbors to frontier
+			for action, state in self.neighbors((node.state)):
+				if not frontier.contains_state((state)) and state not in self.explored:
+					child = Node(state=state, parent=node, action=action)
+					frontier.add(child)
+
 
 print("hello word")
 m = Maze("maze1.txt")
 m.printt()
 
 print(m.goal)
-print(m.neighbors())
+m.solve()
+print(m.solution)
