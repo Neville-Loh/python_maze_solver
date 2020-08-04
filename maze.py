447b65d2588ab1987f815864bf4342832d569da0
Neville <nevilleloh0115@gmail.com>
2020-08-05 11:35:00 +1200

    Added maze.py

---
+++ maze.py
@@ -0,0 +1,131 @@
+import sys
+
+class Node():
+	def __init__(self, state, parent, action):
+		self.state = state
+		self.parent = parent
+		self.action = action
+
+
+class StackFrontier():
+	def __init__(self):
+		self.frontier = []
+
+	def add(self, node):
+		self.frontier.append(node)
+
+	def contains_state(self, state):
+		return any(node.state == state for node in self.frontier)
+
+	def empty(self):
+		return len(self.frontier) == 0
+
+	def remove(self):
+		if self.empty():
+			raise Exception("empty frontier")
+
+		else:
+			node = self.frontier[-1]
+			self.frontier = self.frontier[:-1]
+			return node
+
+class QueueFrontier(StackFrontier):
+
+	def remove(self):
+		if self.empty():
+			raise Exception("empty frontier")
+		else:
+			node = self.frontier[0]
+			self.frontier = self.frontier[1:]
+			return node
+
+
+class Maze():
+
+	def __init__(self,filename):
+		# Read file and set height and width of maze
+		with open(filename) as f:
+			contents = f.read()
+
+		# Validate start and goal
+		if contents.count("A") !=1:
+			raise Exception("maze must have exactly one start point")
+		if contents.count("B") != 1:
+			raise Exception("maze must have exactly one goal")
+
+		# Determine height and width of maze
+		contents = contents.splitlines()
+		self.height = len(contents)
+		self.width = max(len(line) for line in contents)
+
+		# Keep track of walls
+		self.walls = []
+		for i in range(self.height):
+			row = []
+			for j in range(self.width):
+				try:
+					if contents[i][j] == "A":
+						self.start = (i, j)
+						row.append(False)
+
+					elif contents[i][j] == "B":
+						self.goal = (i, j)
+						row.append(False)
+					elif contents[i][j] == " ":
+						row.append(False)
+					else:
+						row.append(True)
+				except IndexError:
+					row.append(False)
+			self.walls.append(row)
+
+		self.solution = None
+
+
+	def printt(self):
+		solution = self.solution[1] if self.solution is not None else None 
+		print()
+		for i, row in enumerate(self.walls):
+			for j, col in enumerate(row):
+				if col:
+					print("#", end="")
+				elif (i, j) == self.start:
+					print("A", end="")
+				elif (i,j) == self.goal:
+					print("B", end="")
+				else:
+					print(" ", end="")
+			print()
+		print()
+
+	def neighbors(self, state):
+		row, col = state
+		# All possible actions
+		candidates = [
+			("up", (row-1, col)),
+			("down", (row + 1, col)),
+			("left", (row, col - 1)),
+			("right", (row, col + 1))
+		]
+
+		# Ensure action are valid
+		result = []
+		for action, (r, c) in candidates:
+			try:
+				if not self.walls[r][c]:
+					result.append((action, (r,c)))
+			except IndexError:
+				continue
+		return result
+
+
+	def solve(self):
+		"""Finds a solution to maze, if one exists."""
+
+
+print("hello word")
+m = Maze("maze1.txt")
+m.printt()
+
+print(m.goal)
+print(m.neighbors())
